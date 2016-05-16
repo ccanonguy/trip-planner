85645c2abd956c444a4f059de09ba77a593c85e3
ccanonguy <singhgurbir10@gmail.com>
2016-05-16 18:30:46 +0530

    extract data

--- start.py
+++ start.py
@@ -1,7 +1,20 @@
 import requests
 import json
+from testFile import *
+from pprintpp import pprint
 from config import *
 
 
-req = requests.post('https://www.googleapis.com/qpxExpress/v1/trips/search', json = payload, headers= {"Content-type": "application/json"}, params = {"key": "AIzaSyDD3kocm1L62YyxEWZNwvjPMdyHefIQQnc"});
-print req.json()
+'''req = requests.post(
+						'https://www.googleapis.com/qpxExpress/v1/trips/search',
+						json = payload,
+						headers = {"Content-type": "application/json"},
+						params = {"key": "AIzaSyDD3kocm1L62YyxEWZNwvjPMdyHefIQQnc"}
+					)'''
+
+dataFlights = p;
+#dataFlights = req.json()
+
+#for option in dataFlights['trips']['tripOption']:
+print pprint(dataFlights['trips']['tripOption'][0], width=1)
+
